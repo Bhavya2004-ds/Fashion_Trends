9ddd1c8d982ce96aa5a1af573ebeefdb00320c72
Bhavya Pradhan <bhavyapradhan8@gmail.com>
2024-08-07 23:30:52 +0530

    third day

--- backend/controllers/products/getproductcontroller.js
+++ backend/controllers/products/getproductcontroller.js
@@ -4,10 +4,40 @@ const ProductCollection=require("../../models/ProductSchema")
 
 const getproductcontroller=async(req,res)=>{
     try{
-        const product = await ProductCollection.find()
-        res.status(200).send(product)
-        console.log('Product fetched successfully')
+        const {category, name, sub_category}=req.params;
+        let products
+        if(category){
 
+            const searchcategory=category;
+            products=await ProductCollection.find({
+                category:{ $regex : new RegExp(searchcategory,'i')}})
+
+        }
+        else if(name){
+
+            const searchname=name.toLowerCase();
+            products=await ProductCollection.find({
+                name:{$regex:new RegExp(searchname,'i')}})
+
+        } 
+        else if(sub_category){
+
+            const searchsubcategory=sub_category.toLowerCase();
+            products=await ProductCollection.find({
+                sub_category:{$regex:new RegExp(searchsubcategory,'i')}})
+
+        }
+     
+         else{
+             products = await ProductCollection.find()
+            console.log('Product fetched successfully')
+         }
+         if(!products || products.length==0){
+            return res.status(404).send({message:"Product not found"})
+         }
+         
+         
+         res.status(200).send(products)
     }
     catch(error){
         res.status(504).send({
--- backend/routes/productroute.js
+++ backend/routes/productroute.js
@@ -2,4 +2,7 @@ const express=require("express")
 const getproductcontroller=require("../controllers/products/getproductcontroller")
 const router=express.Router()
 router.get('/',getproductcontroller)
+router.get('/category/:category',getproductcontroller)
+router.get('/name/:name',getproductcontroller)
+router.get('/sub_category/:sub_category',getproductcontroller)
 module.exports=router
